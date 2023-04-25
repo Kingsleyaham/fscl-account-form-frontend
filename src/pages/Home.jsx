29aba91dbd787b7066507c74ceb2f1b141525bcd
Aham Kingsley <kingsleyaham6@gmail.com>
2023-04-25 05:16:31 +0100

    added redirect to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
+import { useEffect } from "react";
+
 const Home = () => {
+  useEffect(() => {
+    window.location.href = "https://www.fcslng.com/account-opening/";
+  }, []);
   return (
     <div>
-      <p className="text-center text-red-200">Welcome to my home page</p>
+      <p className="text-center">redirecting to account opening page</p>
     </div>
   );
 };
